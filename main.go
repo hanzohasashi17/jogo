2fc3ba0743102ff6d21448fd07c255ae4bf5fa3b
Aidos <nomadbaj@gmail.com>
2024-12-06 01:40:44 +0500

    check work templates

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+  r := gin.Default()
+  r.LoadHTMLGlob("views/*")
+
+  r.GET("/", func(c *gin.Context) {
+    c.HTML(http.StatusOK, "index.html", nil)
+  })
+  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
